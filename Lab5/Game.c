88fc69ea11c1ab0a159f6cf645cbb7891918ad3b
DESKTOP-GK8GIDB\Alex Perera <aperera208@ufl.edu>
2017-11-11 18:15:25 -0500

    erase and draw parts of paddles test

--- Lab5/Game.c
+++ Lab5/Game.c
@@ -204,12 +204,6 @@ void ReadJoystickClient()
         }
         G8RTOS_SignalSemaphore(&PlayerMutex);
 
-        G8RTOS_WaitSemaphore(&GSMutex);
-        GameZ.player = client_info;
-        GameZ.players[Client].currentCenter = client_info.displacement;
-        G8RTOS_SignalSemaphore(&GSMutex);
-
-
         G8RTOS_Sleep(10);
 
     }
@@ -608,33 +602,33 @@ void UpdatePlayerOnScreen(PrevPlayer_t * prevPlayerIn, GeneralPlayerInfo_t * out
 
     if(outPlayer->position == TOP)
     {
-        LCD_DrawRectangle(prevPlayerIn->Center - PADDLE_LEN_D2, prevPlayerIn->Center + PADDLE_LEN_D2, ARENA_MIN_Y, ARENA_MIN_Y + PADDLE_WID , LCD_BLACK);
-        LCD_DrawRectangle(outPlayer->currentCenter - PADDLE_LEN_D2, outPlayer->currentCenter + PADDLE_LEN_D2, ARENA_MIN_Y, ARENA_MIN_Y + PADDLE_WID, client_p1.color);
+        //LCD_DrawRectangle(prevPlayerIn->Center - PADDLE_LEN_D2, prevPlayerIn->Center + PADDLE_LEN_D2, ARENA_MIN_Y, ARENA_MIN_Y + PADDLE_WID , LCD_BLACK);
+        //LCD_DrawRectangle(outPlayer->currentCenter - PADDLE_LEN_D2, outPlayer->currentCenter + PADDLE_LEN_D2, ARENA_MIN_Y, ARENA_MIN_Y + PADDLE_WID, client_p1.color);
         if(offset > 0)
         {
-            //LCD_DrawRectangle(prevPlayerIn->Center - PADDLE_LEN_D2 , prevPlayerIn->Center + offset , ARENA_MIN_Y, ARENA_MIN_Y + PADDLE_WID, LCD_BLACK);
-            //LCD_DrawRectangle(prevPlayerIn->Center + PADDLE_LEN_D2, prevPlayerIn->Center + PADDLE_LEN_D2 + offset, ARENA_MIN_Y, ARENA_MIN_Y + PADDLE_WID, client_p1.color);
+            LCD_DrawRectangle(prevPlayerIn->Center - PADDLE_LEN_D2 , prevPlayerIn->Center + offset , ARENA_MIN_Y, ARENA_MIN_Y + PADDLE_WID, LCD_BLACK);
+            LCD_DrawRectangle(prevPlayerIn->Center + PADDLE_LEN_D2, prevPlayerIn->Center + PADDLE_LEN_D2 + offset, ARENA_MIN_Y, ARENA_MIN_Y + PADDLE_WID, client_p1.color);
         }
         else if (offset < 0)
         {
-            //LCD_DrawRectangle(prevPlayerIn->Center + PADDLE_LEN_D2 - offset, prevPlayerIn->Center + PADDLE_LEN_D2 , ARENA_MIN_Y, ARENA_MIN_Y + PADDLE_WID, LCD_BLACK);
-            //LCD_DrawRectangle(prevPlayerIn->Center - PADDLE_LEN_D2 - offset, prevPlayerIn->Center - PADDLE_LEN_D2 , ARENA_MIN_Y, ARENA_MIN_Y + PADDLE_WID, client_p1.color);
+            LCD_DrawRectangle(prevPlayerIn->Center + PADDLE_LEN_D2 - offset, prevPlayerIn->Center + PADDLE_LEN_D2 , ARENA_MIN_Y, ARENA_MIN_Y + PADDLE_WID, LCD_BLACK);
+            LCD_DrawRectangle(prevPlayerIn->Center - PADDLE_LEN_D2 - offset, prevPlayerIn->Center - PADDLE_LEN_D2 , ARENA_MIN_Y, ARENA_MIN_Y + PADDLE_WID, client_p1.color);
         }
 
     }
     else
     {
-        LCD_DrawRectangle(prevPlayerIn->Center - PADDLE_LEN_D2, prevPlayerIn->Center + PADDLE_LEN_D2, ARENA_MAX_Y-PADDLE_WID, ARENA_MAX_Y , LCD_BLACK);
-        LCD_DrawRectangle(outPlayer->currentCenter - PADDLE_LEN_D2, outPlayer->currentCenter + PADDLE_LEN_D2, ARENA_MAX_Y-PADDLE_WID, ARENA_MAX_Y, host_p0.color);
+        //LCD_DrawRectangle(prevPlayerIn->Center - PADDLE_LEN_D2, prevPlayerIn->Center + PADDLE_LEN_D2, ARENA_MAX_Y-PADDLE_WID, ARENA_MAX_Y , LCD_BLACK);
+        //LCD_DrawRectangle(outPlayer->currentCenter - PADDLE_LEN_D2, outPlayer->currentCenter + PADDLE_LEN_D2, ARENA_MAX_Y-PADDLE_WID, ARENA_MAX_Y, host_p0.color);
         if(offset > 0)
         {
-            //LCD_DrawRectangle(prevPlayerIn->Center - PADDLE_LEN_D2 , prevPlayerIn->Center + offset , ARENA_MAX_Y-PADDLE_WID, ARENA_MAX_Y, LCD_BLACK);
-            //LCD_DrawRectangle(prevPlayerIn->Center + PADDLE_LEN_D2, prevPlayerIn->Center + PADDLE_LEN_D2 + offset, ARENA_MAX_Y-PADDLE_WID, ARENA_MAX_Y, host_p0.color);
+            LCD_DrawRectangle(prevPlayerIn->Center - PADDLE_LEN_D2 , prevPlayerIn->Center + offset , ARENA_MAX_Y-PADDLE_WID, ARENA_MAX_Y, LCD_BLACK);
+            LCD_DrawRectangle(prevPlayerIn->Center + PADDLE_LEN_D2, prevPlayerIn->Center + PADDLE_LEN_D2 + offset, ARENA_MAX_Y-PADDLE_WID, ARENA_MAX_Y, host_p0.color);
         }
         else if (offset < 0)
         {
-            //LCD_DrawRectangle(prevPlayerIn->Center + PADDLE_LEN_D2 - offset, prevPlayerIn->Center + PADDLE_LEN_D2 , ARENA_MAX_Y-PADDLE_WID, ARENA_MAX_Y, LCD_BLACK);
-            //LCD_DrawRectangle(prevPlayerIn->Center - PADDLE_LEN_D2 - offset, prevPlayerIn->Center - PADDLE_LEN_D2 , ARENA_MAX_Y-PADDLE_WID, ARENA_MAX_Y, host_p0.color);
+            LCD_DrawRectangle(prevPlayerIn->Center + PADDLE_LEN_D2 - offset, prevPlayerIn->Center + PADDLE_LEN_D2 , ARENA_MAX_Y-PADDLE_WID, ARENA_MAX_Y, LCD_BLACK);
+            LCD_DrawRectangle(prevPlayerIn->Center - PADDLE_LEN_D2 - offset, prevPlayerIn->Center - PADDLE_LEN_D2 , ARENA_MAX_Y-PADDLE_WID, ARENA_MAX_Y, host_p0.color);
         }
 
     }
