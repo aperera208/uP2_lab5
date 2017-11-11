b7c98fb616c4ee623882a0797e7a2e033ce6a29b
DESKTOP-GK8GIDB\Alex Perera <aperera208@ufl.edu>
2017-11-11 18:10:35 -0500

    joystick client updates gamestate

--- Lab5/Game.c
+++ Lab5/Game.c
@@ -204,6 +204,12 @@ void ReadJoystickClient()
         }
         G8RTOS_SignalSemaphore(&PlayerMutex);
 
+        G8RTOS_WaitSemaphore(&GSMutex);
+        GameZ.player = client_info;
+        GameZ.players[Client].currentCenter = client_info.displacement;
+        G8RTOS_SignalSemaphore(&GSMutex);
+
+
         G8RTOS_Sleep(10);
 
     }
